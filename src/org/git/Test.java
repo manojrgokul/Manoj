fca9421148509e686f2a98013f2b8a3183e248ac
manojrgokul <manojrgokul@gmail.com>
2022-04-12 19:29:18 +0530

    FIRST TEST

---
+++ src/org/git/Test.java
@@ -0,0 +1,20 @@
+package org.git;
+
+public class Test {
+
+	private void employeeName() {
+		System.out.println("MANOJ");
+
+	}
+	private void employeeId() {
+		System.out.println("1000");
+
+	}
+	public static void main(String[] args) {
+		Test e = new Test();
+		e.employeeName();
+		e.employeeId();
+	}
+	}
+
+
